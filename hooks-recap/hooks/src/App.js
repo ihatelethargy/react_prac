1833792ecb756f7868fac2d288eeb694a4c15b12
ihatelethargy <naturalflavor1007@naver.com>
2020-11-19 22:13:04 +0900

    .

--- hooks-recap/hooks/src/App.js
+++ hooks-recap/hooks/src/App.js
@@ -1,11 +1,33 @@
-import './App.css';
+import React, {useState, useEffect, useRef} from "react";
+import ReactDOM from "react-dom";
 
-function App() {
-  return (
-    <>
-      
+const useTitle = (initialTitle) =>{
+  const [title, setTitle] = useState(initialTitle);
+  const titleUpdator = () =>{
+    const titleHtml = document.querySelector('title');
+    titleHtml.innerText = title;
+  }
+  useEffect(titleUpdator, [title]);
+  return setTitle;
+}
+
+const App = () =>{
+  const titleUpdate = useTitle("Loading...");
+  setTimeout(()=>titleUpdate("home"),3000);
+  return(
+    <> 
+      <h1>hi</h1>
     </>
   );
 }
 
+
+
 export default App;
+
+
+
+
+
+
+
