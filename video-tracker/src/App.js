65b44af25506dd7f2363ff22662eabbf66cca56b
ihatelethargy <naturalflavor1007@naver.com>
2020-11-20 02:35:58 +0900

    connect youtube API + postman

---
+++ video-tracker/src/App.js
@@ -0,0 +1,25 @@
+import { useState,useEffect } from 'react';
+import './App.css';
+import VideoList from './components/video_list/video_list';
+
+function App() {
+  const [videos, setVideos] = useState([]);
+
+
+  useEffect(()=>{
+    let requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDmklK6OYBIL-AwZ4lBTkTj0iuZ7tGS2Yw", requestOptions)
+      .then(response => response.json())
+      .then(result => {setVideos(result.items); console.log(result)})
+      .catch(error => console.log('error', error));
+  }, [])
+  return (
+    <VideoList videos={videos} />
+  );
+}
+
+export default App;
