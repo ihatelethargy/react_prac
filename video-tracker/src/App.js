e24a843ccbca77533d5e27da426328fa011f1c0c
ihatelethargy <naturalflavor1007@naver.com>
2020-11-21 00:25:29 +0900

    search + postman

--- video-tracker/src/App.js
+++ video-tracker/src/App.js
@@ -1,12 +1,24 @@
 import { useState,useEffect } from 'react';
-import './App.css';
+import './app.css';
 import Navbar from './components/navbar/navbar';
 import VideoList from './components/video_list/video_list';
 
 function App() {
   const [videos, setVideos] = useState([]);
 
-
+  const search = query => {
+    
+    var requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyDmklK6OYBIL-AwZ4lBTkTj0iuZ7tGS2Yw`, requestOptions)
+      .then(response => response.json())
+      .then(result => result.items.map(item=>({...item, id: item.id.videoId})))
+      .then(items => setVideos(items))
+      .catch(error => console.log('error', error));
+  }
   useEffect(()=>{
     let requestOptions = {
       method: 'GET',
@@ -15,14 +27,14 @@ function App() {
     
     fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDmklK6OYBIL-AwZ4lBTkTj0iuZ7tGS2Yw", requestOptions)
       .then(response => response.json())
-      .then(result => {setVideos(result.items); console.log(result)})
+      .then(result => {setVideos(result.items);})
       .catch(error => console.log('error', error));
   }, [])
   return (
-    <>
-      <Navbar/>
+    <div className="app">
+      <Navbar onSearch={search}/>
       <VideoList videos={videos} />
-    </>
+    </div>
   );
 }
 
--- video-tracker/src/components/navbar/navbar.jsx
+++ video-tracker/src/components/navbar/navbar.jsx
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './navbar.css'
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
-const Navbar = (props) => (
-            
-        <div className="navbar">
-            <h1 className="navbar-title">SOPTUBE</h1>
-            <form action=""></form>
-            <div className="info">info</div>
-        </div>
-            
-);
+
+
+const Navbar = (props) => {
+    
+    const inputRef = useRef();
+
+    const handleSearch = () => {
+        const value = inputRef.current.value;
+        
+        props.onSearch(value);
+    }
+
+    const onClick = (event) =>{
+        handleSearch()
+    }
+    
+    const onKeyPress = (event) => {
+        if(event.key === 'Enter') handleSearch()
+    }    
+    return(
+            <div className="navbar">
+                <h1 className="navbar-title">SOPTUBE</h1>
+                <div className="searchForm">
+                    <input className="search" 
+                        ref={inputRef}   
+                        type="text" 
+                        placeholder="search..."
+                        onKeyPress={onKeyPress} 
+                        />
+                    <button className="searchBtn"
+                            onClick={onClick}
+                    ><img src="/images/search.png" alt=""/></button>
+                </div>
+                <div className="info">info</div>
+            </div>
+    );
+}
 
 export default Navbar;
