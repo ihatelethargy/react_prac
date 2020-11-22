4bb42020de47eb66f9659b6adec47247d33704e2
ihatelethargy <naturalflavor1007@naver.com>
2020-11-22 17:39:01 +0900

    env git ignore추가

--- video-tracker/src/App.js
+++ video-tracker/src/App.js
@@ -3,33 +3,20 @@ import './app.css';
 import Navbar from './components/navbar/navbar';
 import VideoList from './components/video_list/video_list';
 
-function App() {
+function App({ youtube }) {
   const [videos, setVideos] = useState([]);
-
   const search = query => {
-    
-    var requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-    
-    fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyDmklK6OYBIL-AwZ4lBTkTj0iuZ7tGS2Yw`, requestOptions)
-      .then(response => response.json())
-      .then(result => result.items.map(item=>({...item, id: item.id.videoId})))
-      .then(items => setVideos(items))
-      .catch(error => console.log('error', error));
+    youtube
+    .search(query)
+    .then(videos => setVideos(videos))
   }
-  useEffect(()=>{
-    let requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
     
-    fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDmklK6OYBIL-AwZ4lBTkTj0iuZ7tGS2Yw", requestOptions)
-      .then(response => response.json())
-      .then(result => {setVideos(result.items);})
-      .catch(error => console.log('error', error));
+  useEffect(()=>{
+    youtube
+    .mostPopular()
+    .then(videos => setVideos(videos))
   }, [])
+  
   return (
     <div className="app">
       <Navbar onSearch={search}/>
